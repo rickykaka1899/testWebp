ba0e8df2316e7b12ac389cf59494590de9f74d01
houlzh <houlzh@yonyou.com>
2015-01-13 13:44:48 +0800

    Initial Commit

---
+++ WebpTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WebpTest
+//
+//  Created by KakaCompany on 13/1/15.
+//  Copyright (c) 2015 KakaCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WebpTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebpTest
+//
+//  Created by KakaCompany on 13/1/15.
+//  Copyright (c) 2015 KakaCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
